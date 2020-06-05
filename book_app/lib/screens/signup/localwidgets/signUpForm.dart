c96c2185167ddb6f221e900620d099a6b0cb74c7
anubhav11803451 <anubhav.11803451@lpu.in>
2020-06-05 18:15:30 +0530

    Adding UI for Log In and Sign Up

---
+++ book_app/lib/screens/signup/localwidgets/signUpForm.dart
@@ -0,0 +1,78 @@
+import 'package:book_app/widgets/ourContainer.dart';
+import 'package:flutter/material.dart';
+
+class OurSignUpForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OurContainer(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 8.0),
+            child: Text(
+              "Sign Up",
+              style: TextStyle(
+                color: Theme.of(context).secondaryHeaderColor,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 15,),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.person_outline),
+              hintText: "Full Name",
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.alternate_email),
+              hintText: "Email",
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock_outline),
+              hintText: "Password",
+            ),
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock_outline),
+              hintText: "Confirm Password",
+            ),
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          RaisedButton(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 100),
+              child: Text(
+                "Sign Up",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
