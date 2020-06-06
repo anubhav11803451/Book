7a2d4150108e0530e6e35ab81eef420f69d71c29
anubhav11803451 <anubhav.11803451@lpu.in>
2020-06-06 18:43:37 +0530

    Added more UI Screen (Building UI)

---
+++ book_app/lib/screens/noGroup/noGroup.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class OurNoGroup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    void _goToJoin() {}
+    void _goToCreate() {}
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Spacer(
+            flex: 1,
+          ),
+          Padding(
+            padding: EdgeInsets.all(80.0),
+            child: Image.asset("assets/images/book.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: Text(
+              "Welcome to Book App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Text(
+              "Since you are not in a book app, you can select either " +
+                  "to join a club or create a club.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          Container(
+            padding: const EdgeInsets.all(30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: RaisedButton(
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    child: Text("Create"),
+                    onPressed: () => _goToCreate(),
+                    color: Theme.of(context).canvasColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(
+                        color: Theme.of(context).secondaryHeaderColor,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: RaisedButton(
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    child: Text(
+                      "  Join  ",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () => _goToJoin(),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
